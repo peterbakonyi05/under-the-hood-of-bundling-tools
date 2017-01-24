ff2cc825ecf413e59095cfb03e24e2bbd525d1b9
Peter Bakonyi <peter.bakonyi@betssongroup.com>
2017-01-24 17:55:13 +0100

    wip

--- src/cjs-naive-bundler.js
+++ src/cjs-naive-bundler.js
@@ -2,12 +2,61 @@ const path = require('path');
 const fs = require("fs");
 var mdeps = require('module-deps');
 var JSONStream = require('JSONStream');
-const t = require('through2');
+const through = require('through');
 
-var md = mdeps();
-md
-    .pipe(JSONStream.stringify())
-    // todo: transform files...
-    .pipe(fs.createWriteStream("bundle.js"));
+/**
+ * Get the entry file and 
+ * 
+ * @returns [{id: string, source: string, deps: {}, file: string, [entry]: boolean }]
+ * 
+ * @example
+ * [
+ *  {
+ *      "id":"c:\\Github\\demystify-bundling\\example\\calculator-cjs\\calculator.js",
+ *      "source":"module.exports = {\r\n\tadd: function (a, b) {\r\n\t\treturn a + b;\r\n\t}\r\n};",
+ *      "deps":{}
+ *      "file":"c:\\Github\\demystify-bundling\\example\\calculator-cjs\\calculator.js"
+ *  },
+ *  {
+ *      "file":"c:\\Github\\demystify-bundling\\example\\calculator-cjs\\index.js",
+ *      "id":"c:\\Github\\demystify-bundling\\example\\calculator-cjs\\index.js",
+ *      "source":"const calculator = require(\"./calculator\");\n\nconsole.log(calculator.add(1, 2));",
+ *      "deps":{"./calculator":"c:\\Github\\demystify-bundling\\example\\calculator-cjs\\calculator.js"},
+ *      "entry":true
+ *  }
+ * ]
+ */
+function getModuleDependencies(entry) {
+    return new Promise(resolve => {
+        const dependencies = [];
+        const md = mdeps();
+        md.pipe(through(
+            function (data) {
+                dependencies.push(data);
+                this.queue(data);
+            },
+            function () { //optional
+                const map = 
+                resolve(dependencies);
+                this.queue(null);
+            })
+        )
+        md.on("tranform", (tr, file) => {
+            console.log(tr, file);
+            return file;
+        });
+        md.end({ file: entry });
+    });
+}
 
-md.end({ file: path.join(__dirname, '../example/calculator-cjs/index.js')});
+function bundle(entry) {
+    getModuleDependencies(entry)
+        .then(dependencies => {
+            console.log(dependencies);
+        })
+}
+
+bundle(path.join(__dirname, '../example/calculator-cjs/index.js'));
+
+// todo: transform files...
+// .pipe(fs.createWriteStream("bundle.js"));
