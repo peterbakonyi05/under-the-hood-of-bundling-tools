119e56205d337a1d0ad2ee27b89114d1b62bb197
peterbakonyi05 <peter.bakonyi05@gmail.com>
2017-02-22 21:13:56 +0100

    fix formatting

--- bin/fuse-box-cjs.js
+++ bin/fuse-box-cjs.js
@@ -1,9 +1,9 @@
 const path = require("path");
 const FuseBox = require("fuse-box").FuseBox;
 
-const fuseBox = FuseBox.init({ 
-  homeDir : path.resolve("example/calculator-cjs"),
-  outFile : path.resolve("output/calculator-fuse-box-cjs-bundle.js")
+const fuseBox = FuseBox.init({
+  homeDir: path.resolve("example/calculator-cjs"),
+  outFile: path.resolve("output/calculator-fuse-box-cjs-bundle.js")
 })
 
 fuseBox.bundle(">index.js");
